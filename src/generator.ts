0308c1d5c67d8a5daeb7d944bbfcc893d5508954
faissaloux <fwahabali@gmail.com>
2024-10-28 17:31:45 +0100

    refactor generator constructor

--- src/generator.ts
+++ src/generator.ts
@@ -1,12 +1,9 @@
 import fakeData from "./data";
 
 class Generator {
-    private value: string;
     private data: {[key: string]: string} = fakeData();
 
-    constructor(value: string) {
-        this.value = value;
-    }
+    constructor(private value: string) {}
 
     generate() {
         return this.data[this.value] ? this.data[this.value] : this.value;
