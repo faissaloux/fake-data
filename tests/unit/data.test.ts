d7f7fd8359d2a0510b207f38c66a4ca7bb2dcf8a
faissaloux <fwahabali@gmail.com>
2024-10-28 15:53:17 +0100

    add more fakeData content tests

--- tests/unit/data.test.ts
+++ tests/unit/data.test.ts
@@ -2,12 +2,19 @@ import fakeData from "../../src/data";
 
 describe('data', () => {
     test('generates valid option', () => {
-        const dataKeys = Object.keys(fakeData());
+        const data: {[key: string]: string} = fakeData();
+        const dataKeys: string[] = Object.keys(data);
 
         expect(dataKeys).toContain('location.city');
         expect(dataKeys).toContain('person.firstName');
         expect(dataKeys).toContain('person.male.firstName');
         expect(dataKeys).toContain('person.female.firstName');
         expect(dataKeys).toContain('person.lastName');
+
+        expect(data['location.city'].length).toBeGreaterThan(1);
+        expect(data['person.firstName'].length).toBeGreaterThan(1);
+        expect(data['person.male.firstName'].length).toBeGreaterThan(1);
+        expect(data['person.female.firstName'].length).toBeGreaterThan(1);
+        expect(data['person.lastName'].length).toBeGreaterThan(1);
     });
 });
