398944e1d316304b7ef4109667b9c2a098a9cbba
faissaloux <fwahabali@gmail.com>
2024-12-03 10:00:17 +0100

    lorem.paragraph

--- src/dataTypes/lorem.ts
+++ src/dataTypes/lorem.ts
@@ -1,8 +1,23 @@
 import { faker } from '@faker-js/faker';
 
+type ParagraphParams =
+    | number
+    | {
+        min: number;
+        max: number;
+    };
+
 class Lorem {
     text(): string {
         return faker.lorem.text();
+    }
+
+    paragraph(sentenceCount: ParagraphParams = 3): string {
+        if (typeof sentenceCount !== 'number' && typeof sentenceCount !== 'object') {
+            sentenceCount = 3;
+        }
+
+        return faker.lorem.paragraph(sentenceCount);
     }
 }
 
